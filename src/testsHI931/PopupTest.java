dc6ec48a11f1b269ae4aa9df3020a22ae8270aaf
Piotr Krytkowski <krtp@viessmann.com>
2015-12-03 15:08:29 +0100

    Fix for popup tests

--- src/testsHI931/PopupTest.java
+++ src/testsHI931/PopupTest.java
@@ -17,9 +17,11 @@ public class PopupTest {
 		basicFunction.goToMainMenu();
 		basicFunction.insertValueToHI931(s,Const.faultPopup);
 		if (basicFunction.checkIfExist("FaultPopup.JPG")){
+			basicFunction.clickIfExist("ConfirmButton.JPG");
 			result = true;
 			System.out.println("Fault popup is correct.");			
 			System.out.println("Test: Success" + '\n' + log.separator);
+			
 		}else{
 			System.out.println("Fault popup is not correct.");
 			System.out.println("Test: Fail" + '\n' + log.separator);
