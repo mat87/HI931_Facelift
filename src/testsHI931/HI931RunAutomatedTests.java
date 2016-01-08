e7091d970ff45ef0765f4819487c5d301a2e3d82
Mateusz Gradzik <grzm@viessmann.com>
2016-01-08 14:47:08 +0100

    added new screen

--- src/testsHI931/HI931RunAutomatedTests.java
+++ src/testsHI931/HI931RunAutomatedTests.java
@@ -28,13 +28,13 @@ public class HI931RunAutomatedTests {
 		Thread.sleep(3000);
 	}
 
-	@Test(priority = 1)
+	//@Test(priority = 1)
 	public void testCheckOperatingProgramHeader() throws InterruptedException{
 		boolean result = opt.checkOperatingProgramHeader();
 		Assert.assertEquals(result, true);
 	}
 	
-	@Test(priority = 2)
+	//@Test(priority = 2)
 	public void testCheckOperatingProgramFooter() throws InterruptedException{
 		boolean result = opt.checkOperatingProgramFooter();
 		Assert.assertEquals(result, true);
@@ -46,31 +46,31 @@ public class HI931RunAutomatedTests {
 		Assert.assertEquals(result, true);
 	}
 	
-	@Test(priority = 4)
+	//@Test(priority = 4)
 	public void testSetRoomTemperatureHighest() throws InterruptedException{
 		boolean result = rtt.setRoomTemperatureHighest();
 		Assert.assertEquals(result, true);
 	}
 	
-	@Test(priority = 5)
+	//@Test(priority = 5)
 	public void testSetRoomTemperatureLowest() throws InterruptedException{
 		boolean result = rtt.setRoomTemperatureLowest();
 		Assert.assertEquals(result, true);
 	}
 	
-	@Test(priority = 6)
+	//@Test(priority = 6)
 	public void testCheckRoomTemperatureHeader() throws InterruptedException{
 		boolean result = rtt.checkRoomTemperatureHeader();
 		Assert.assertEquals(result, true);
 	}
 	
-	@Test(priority = 7)
+	//@Test(priority = 7)
 	public void testCheckOutsideTemperature() throws InterruptedException{
 		boolean result = ott.checkOutsideTemperature();
 		Assert.assertEquals(result, true);
 	}
 	
-	@Test(priority = 8)
+	//@Test(priority = 8)
 	public void testCheckBoilerTemperature() throws InterruptedException{
 		boolean result = btt.checkBoilerTemperature();
 		Assert.assertEquals(result, true);
