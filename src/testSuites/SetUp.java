e02ce2b08e072828a8a3e013f928a551c9ced619
Mateusz Gradzik <grzm@viessmann.com>
2016-01-13 11:39:41 +0100

    SetUp file added

---
+++ src/testSuites/SetUp.java
@@ -0,0 +1,26 @@
+package testSuites;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import automatedTestFramework.BasicFunctions;
+
+public class SetUp {
+	
+	BasicFunctions func = new BasicFunctions();
+	
+	String path = "C:\\Workspace\\viessmann-hi-931\\build\\bin\\";
+	String app = "simulator.exe";
+	
+	@BeforeTest
+	public void setUp() throws FileNotFoundException, IOException, InterruptedException{
+		func.runApplication(path, app);
+		Thread.sleep(3000);
+	}
+	
+	@AfterTest
+	public void tearDown() throws IOException{
+		func.killApplication(app);
+	}
+}
